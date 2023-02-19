2de7f76ba3021b413604779aa82674957d098f7f
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-02-19 06:37:39 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by HEMANTH on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
